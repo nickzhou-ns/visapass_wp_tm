38c6a0fb7bf2659b06db88e4b14a010aab9b14a8
mingqiang <1165109730@qq.com>
2024-12-11 14:26:22 +0800

    first

--- index.php
+++ index.php
@@ -38,11 +38,6 @@ $blog_column = is_active_sidebar( 'blog-sidebar' ) ? 8 : 12 ;
 							while ( have_posts() ) :
 								the_post(); ?>
 								<?php
-								/*
-								 * Include the Post-Type-specific template for the content.
-								 * If you want to override this in a child theme, then include a file
-								 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-								 */
 								get_template_part( 'template-parts/content', get_post_format() ); ?>					
 							<?php
 							endwhile;
@@ -60,7 +55,6 @@ $blog_column = is_active_sidebar( 'blog-sidebar' ) ? 8 : 12 ;
 
 					endif;
 					?>
-
 				</div>
 			</div>
 
